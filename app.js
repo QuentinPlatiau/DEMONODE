a6c24ac9bde3290dfa0695d62d07fe083a6edc54
Quentin <quentin.platiau@student.vinci.be>
2023-10-12 19:41:11 +0200

    refactor if minutes return '1 block jaune

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 export class BerlinClock {
     minuteLine(time){
         if(Math.floor(time.getMinutes()/5) === 0) return "0 block jaune";                       
-        if(time.getMinutes() === 5) return "1 block jaune";
-        if(time.getMinutes() === 6) return "1 block jaune";
+        if(Math.floor(time.getMinutes()/5) === 1) return "1 block jaune";
     }
 }
 
