dbba28c0379b01bb916e8ac83756c9c95ac5b5c1
Quentin <quentin.platiau@student.vinci.be>
2023-10-12 20:15:20 +0200

    add method minuteLine

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -155,4 +155,59 @@ describe("the first minute line", function() {
         //assert
         expect(result).toBe("8 block jaune et 3 block rouge");
     });
+
+    it("minuteLine given 0.00 should return 0 block jaune", function(){
+        //arrange
+        let time = new Date('March 13, 08 00:00:00');
+        //act
+        let result = berlinClock.minuteLine(time);
+        console.log(result);
+        //assert
+        expect(result).toBe("0 block jaune");
+    });
+    it("minuteLine given 1.00 should return 0 block jaune", function(){
+        //arrange
+        let time = new Date('March 13, 08 00:01:00');
+        //act
+        let result = berlinClock.minuteLine(time);
+        console.log(result);
+        //assert
+        expect(result).toBe("1 block jaune");
+    });
+    it("minuteLine given 2.00 should return 0 block jaune", function(){
+        //arrange
+        let time = new Date('March 13, 08 00:02:00');
+        //act
+        let result = berlinClock.minuteLine(time);
+        console.log(result);
+        //assert
+        expect(result).toBe("2 block jaune");
+    });
+    it("minuteLine given 3.00 should return 0 block jaune", function(){
+        //arrange
+        let time = new Date('March 13, 08 00:03:00');
+        //act
+        let result = berlinClock.minuteLine(time);
+        console.log(result);
+        //assert
+        expect(result).toBe("3 block jaune");
+    });
+    it("minuteLine given 4.00 should return 0 block jaune", function(){
+        //arrange
+        let time = new Date('March 13, 08 00:04:00');
+        //act
+        let result = berlinClock.minuteLine(time);
+        console.log(result);
+        //assert
+        expect(result).toBe("4 block jaune");
+    });
+    it("minuteLine given 5.00 should return 1 block jaune", function(){
+        //arrange
+        let time = new Date('March 13, 08 00:05:00');
+        //act
+        let result = berlinClock.minuteLine(time);
+        console.log(result);
+        //assert
+        expect(result).toBe("0 block jaune");
+    });
 })
