0314bd15873a92d03ada471dda7f98815c94526f
Quentin <quentin.platiau@student.vinci.be>
2023-10-12 19:29:38 +0200

    test minutes === 1 && minutes === 2

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -2,9 +2,27 @@ import { BerlinClock } from '../app.js';
 
 describe("the first minute line", function() {
     let berlinClock = new BerlinClock();
-    it("minuteLine given 0.00 should return blabla", function(){
+    it("minuteLine given 0.00 should return 0 block jaune", function(){
         //arrange
         let time = new Date('March 13, 08 00:00:00');
+        //act
+        let result = berlinClock.minuteLine(time);
+        console.log(result);
+        //assert
+        expect(result).toBe("0 block jaune");
+    });
+    it("minuteLine given 1.00 should return 0 block jaune", function(){
+        //arrange
+        let time = new Date('March 13, 08 00:01:00');
+        //act
+        let result = berlinClock.minuteLine(time);
+        console.log(result);
+        //assert
+        expect(result).toBe("0 block jaune");
+    });
+    it("minuteLine given 2.00 should return 0 block jaune", function(){
+        //arrange
+        let time = new Date('March 13, 08 00:02:00');
         //act
         let result = berlinClock.minuteLine(time);
         console.log(result);
