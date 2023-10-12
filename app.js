597495541d49b4d17250abdd5b0537f820744688
Quentin <quentin.platiau@student.vinci.be>
2023-10-12 20:33:44 +0200

    refactor method fiveMinuteLine for all block

--- app.js
+++ app.js
@@ -3,16 +3,8 @@ export class BerlinClock {
         return (time.getMinutes()%5)+" block jaune";
     }
     fiveMinuteLine(time){
-        if(Math.floor(Math.floor(time.getMinutes()/5)/3) === 0) return (Math.floor(time.getMinutes()/5)) + " block jaune";   
-        if(Math.floor(time.getMinutes()/5) === 3) return "2 block jaune et 1 block rouge";                       
-        if(Math.floor(time.getMinutes()/5) === 4) return "3 block jaune et 1 block rouge";                       
-        if(Math.floor(time.getMinutes()/5) === 5) return "4 block jaune et 1 block rouge";                       
-        if(Math.floor(time.getMinutes()/5) === 6) return "4 block jaune et 2 block rouge";                       
-        if(Math.floor(time.getMinutes()/5) === 7) return "5 block jaune et 2 block rouge";                       
-        if(Math.floor(time.getMinutes()/5) === 8) return "6 block jaune et 2 block rouge";                       
-        if(Math.floor(time.getMinutes()/5) === 9) return "6 block jaune et 3 block rouge";                       
-        if(Math.floor(time.getMinutes()/5) === 10) return "7 block jaune et 3 block rouge";                      
-        if(Math.floor(time.getMinutes()/5) === 11) return "8 block jaune et 3 block rouge";
+        if(Math.floor(Math.floor(time.getMinutes()/5)/3) === 0) return (Math.floor(time.getMinutes()/5)) + " block jaune";
+        return (Math.floor(time.getMinutes()/5) - Math.floor(Math.floor(time.getMinutes()/5)/3)) + " block jaune et "+ Math.floor(Math.floor(time.getMinutes()/5)/3) +" block rouge";
     }
 }
 
